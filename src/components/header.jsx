397e699b31c4d1974467fb2778b4bb981dd17ff3
sosamandara <angelomandara@outlook.it>
2024-11-17 17:05:24 +0100

    solve the problem with the small screen size header

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -4,28 +4,24 @@ import "./header.css";
 export const Header = (props) => {
   return (
     <header id="header">
-      <div className="intro">
-        <div className="overlay">
-          <div className="container">
-            <div className="row">
-              <div className="col-md-8 col-md-offset-2 intro-text">
-                <h1 className="Header-title">
-                  {props.data ? props.data.title : "Loading..."}
-                </h1>
-                <p className="paragraph">
-                  {props.data ? props.data.paragraph : "Loading..."}
-                </p>
-                <a
-                  href="#BookWithUs" /* Updated link to target the "BookWithUs" section */
-                  className="btn btn-custom btn-lg page-scroll"
-                >
-                  Book Now {/* Updated button text */}
-                </a>
-              </div>
-            </div>
-          </div>
-        </div>
+  <div className="intro">
+    <div className="overlay"></div>
+    <div className="container">
+      <div className="intro-text">
+        <h1 className="Header-title">
+          {props.data ? props.data.title : "Welcome to Casa Carmela"}
+        </h1>
+        <p className="paragraph">
+          {props.data
+            ? props.data.paragraph
+            : "Your serene escape nestled in the heart of Positano, along the stunning Amalfi Coast. From cozy spaces to a fully equipped kitchen, our home is designed to make you feel at ease."}
+        </p>
+        <a href="#BookWithUs" className="btn btn-custom">
+          Book Now
+        </a>
       </div>
-    </header>
+    </div>
+  </div>
+</header>
   );
 };
